d6e81720cbe08dfae763da6930898c962c43e629
comp <617086902@qq.com>
2018-01-22 11:53:44 +0800

    first commit

---
+++ Framework/Core/Utilities/NBFS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities
+{
+    public class NBFS
+    {
+        private bool[] marked;
+        private int[] edgeTo;
+        private readonly int s;
+        public NBFS(NGraph g, int s)
+        {
+            marked = new bool[g.V];
+            edgeTo = new int[g.V];
+            this.s = s;
+
+        }
+        private void bfs(NGraph g, int s)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(s);
+            while (queue.Any())
+            {
+                int v = queue.Dequeue();
+                int[] adj = g.Adj(v);
+                for (int i = 0; i < adj.Length; i++)
+                {
+                    int w = adj[i];
+                    if (!marked[w])
+                    {
+                        marked[w] = true;
+                        edgeTo[w] = v;
+                        queue.Enqueue(w);
+                    }
+                }
+            }
+        }
+        public bool HasPathTo(int v)
+        {
+            return marked[v];
+        }
+        public Stack<int> PathTo(int v)
+        {
+            if (!HasPathTo(v)) return null;
+            Stack<int> pathStack = new Stack<int>();
+            for (int x = v; x != s; x = edgeTo[x])
+            {
+                pathStack.Push(x);
+            }
+            return pathStack;
+        }
+    }
+}
---
+++ Framework/Core/Utilities/NGraph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities
+{
+    public class NGraph
+    {
+        private int _v, _e;
+        private int[][] _adj;
+        public NGraph(int v)
+        {
+            _v = v;
+            _e = 0;
+        }
+        public void AddEdge(int v, int w)
+        {
+            _adj[v][_adj[v].Length] = w;
+            _adj[w][_adj[w].Length] = v;
+            _e++;
+        }
+        public int[] Adj(int v)
+        {
+            return _adj[v];
+        }
+        public int V { get { return _v; } }
+        public int E { get { return _e; } }
+    }
+}
